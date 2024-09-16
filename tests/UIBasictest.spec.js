87b75cec77b10dcfbd58849444a7f7a2507207af
Mihail <Mihail.Lecari@advania.co.uk>
2024-09-16 22:12:56 +0100

    first_elementInfo_commit

--- tests/UIBasictest.spec.js
+++ tests/UIBasictest.spec.js
@@ -36,17 +36,36 @@ test('First Playwright test 2', async function({page})   // alternative way to a
 
 test.only('First Playwright test 3', async function({page})
 {
+    const userName = page.locator('#username');
+    const signIn = page.locator('#signInBtn');
+    const password =  page.locator("[type='password']");
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
     let title = await page.title();
     console.log(`The title of the page is: ${title}`);
     await expect(page).toHaveTitle("LoginPage Practise | Rahul Shetty Academy");
-    await page.locator('#username').fill('mlecari');
-    await page.locator("[type='password']").fill('Learning123!');
-    await page.locator('#signInBtn').click();
+    await userName.fill('mlecari');
+    await password.fill('Learning123!');
+    await signIn.click();
+    test.setTimeout(60000);
     await page.locator("[style*='block']").textContent(); // exctracting method, grab the text 
+
+
 
 // also we can use assertions - await expect(locator).toContainText()
 
-    console.log(await page.locator("[style*='block']").textContent()); // we are using this style css structure because "style*" it could be NONE ot BLOCk, either basically
-    await expect(page.locator("[style*='block']")).toContainText('Incorrectt') // it will fail
+    console.log(await page.locator("[style*='block']").textContent()); // we are using this style css structure because "style*" it could be NONE or BLOCk, either basically
+    //await expect(page.locator("[style*='block']")).toContainText('Incorrect') // it will fail
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect') // it will pass
+    test.setTimeout(500000);
+    await userName.fill("");
+    await userName.fill("rahulshettyacademy");
+    await password.fill("");
+    await password.fill("learning");
+    await signIn.click();
+    test.setTimeout(500000);
+    //console.log(await page.locator(".card-body a").first.textContent());   // it will ge t the first element in the array
+    console.log(await page.locator(".card-body a").nth(0).textContent());
+    console.log(await page.locator(".card-body a").nth(1).textContent());
+    console.log(await page.locator(".card-body a").nth(2).textContent());
+    console.log(await page.locator(".card-body a").nth(3).textContent());
 })
